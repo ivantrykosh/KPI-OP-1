abf8aac22846f68dc5152ca36d612a8994936342
Ivan Trykosh <89276102+ivantrykosh@users.noreply.github.com>
2021-10-11 11:54:50 +0300

    Add files via upload

---
+++ OP-Laba4/Laba-C++/Laba-C++.cpp
@@ -0,0 +1,24 @@
+﻿// Задані натуральні числа m та n. Обчислити суму m останніх цифр числа n.
+#include <iostream>
+
+using namespace std;
+
+int main()
+{   
+    // Оголошуємо змінні
+    unsigned long m, n, S;
+
+    // Вводимо дані
+    cout << "Input n: ";
+    cin >> n;
+    cout << "Input m: ";
+    cin >> m;
+    S = 0;
+
+    // Обчислюємо суму
+    for (int i = 0; i < m; i++) {
+        S += n % 10;
+        n = n / 10;
+    }
+    cout << "Answer: " << S;
+}
